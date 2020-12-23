2eaef147730d226978603951e1e3e7a4e36f013a
boltex33 <laca_rey@yahoo.com>
2020-12-23 21:35:36 +0200

    Updates

---
+++ precache-manifest.8da16b484425622663f1bcc4ce21bd7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9b9e810cce62f5651dd7d08d34b3f7",
+    "url": "/boltex33.github.io/index.html"
+  },
+  {
+    "revision": "78e717764369cf24ccb0",
+    "url": "/boltex33.github.io/static/css/2.7550f3b0.chunk.css"
+  },
+  {
+    "revision": "3005a823d97e850dc62c",
+    "url": "/boltex33.github.io/static/css/main.f810c7bf.chunk.css"
+  },
+  {
+    "revision": "78e717764369cf24ccb0",
+    "url": "/boltex33.github.io/static/js/2.7afc3459.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/boltex33.github.io/static/js/2.7afc3459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3005a823d97e850dc62c",
+    "url": "/boltex33.github.io/static/js/main.bd38cdf0.chunk.js"
+  },
+  {
+    "revision": "e9a18a0b195bc3e85f1a",
+    "url": "/boltex33.github.io/static/js/runtime-main.e6293bbe.js"
+  }
+]);
